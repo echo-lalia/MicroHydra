f24283bc31b787c2968ad2b5765a2a8cfb2dad1f
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-03-13 21:10:32 -0700

    expanded keywords

--- MicroHydra/launcher/HyDE.py
+++ MicroHydra/launcher/HyDE.py
@@ -263,8 +263,9 @@ def draw_rules(line,x,y,small=False,highlight=False):
 
 def draw_fancy_line(line, x, y, highlight=False, trim=True):
     """apply special styling to a line and display it."""
-    _KEYWORDS = const(("is","not","and","if","else","elif","return","or","break","as","in","False","True",
-                 "None","try","except","def","global","class","while","for","from","import","self","with"))
+    _KEYWORDS = const(('and','as','assert','break','class','continue','def','del','elif','else','except',
+                       'False','Finally','for','from','global','if','import','in','is','lambda','None',
+                       'nonlocal','not','or','pass','raise','return','True','try','while','with','yield'))
     _OPERATORS = const("<>,|[]{}()*^%!=-+/:;&@")
     # TODO: I worry this may be extremely unoptomized. Should maybe be tested/optimized further.
     
