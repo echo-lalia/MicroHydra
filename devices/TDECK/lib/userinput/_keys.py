92024e77dc024955490f1908150322c213d16575
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-06-20 15:31:38 -0700

    Update _keys.py

--- devices/TDECK/lib/userinput/_keys.py
+++ devices/TDECK/lib/userinput/_keys.py
@@ -13,26 +13,24 @@ KEYMAP = {
     1:'q',   2:'w',  17:'e',  33:'r', 35:'t', 51:'y', 49:'u', 67:'i', 65:'o', 20:'p',
     4:'a',   18:'s', 19:'d',  39:'f', 34:'g', 50:'h', 55:'j', 71:'k', 66:'l', 68:'BSPC',
     5:'ALT',22:'z', 21:'x',  38:'c', 37:'v', 53:'b', 54:'n', 70:'m', 69:'$', 52:'ENT',
-            23:'LSHFT', 7:'CTRL',     6:'SPC',     3:'FN', 36:'SHFT',
+                          7:'CTRL',     6:'SPC',
     }
-KEYMAP_SHFT = KEYMAP.copy()
-KEYMAP_SHFT.update({
+KEYMAP_SHFT = {
     1:'Q',   2:'W',  17:'E',  33:'R', 35:'T', 51:'Y', 49:'U', 67:'I', 65:'O', 20:'P',
-    4:'A',  18:'S',  19:'D',  39:'F', 34:'G', 50:'H', 55:'J', 71:'K', 66:'L',
-            22:'Z',  21:'X',  38:'C', 37:'V', 53:'B', 54:'N', 70:'M',
-    })
-KEYMAP_FN = KEYMAP.copy()
-KEYMAP_FN.update({
+    4:'A',  18:'S',  19:'D',  39:'F', 34:'G', 50:'H', 55:'J', 71:'K', 66:'L', 68:'BSPC',
+    5:'ALT',22:'Z',  21:'X',  38:'C', 37:'V', 53:'B', 54:'N', 70:'M', 69:'$', 52:'ENT',
+                          7:'CTRL',     6:'SPC',
+    }
+KEYMAP_FN = {
     1:'#',   2:'1',  17:'2',  33:'3', 35:'(', 51:')', 49:'_', 67:'-', 65:'+', 20:'@',
     4:'*',  18:'4',  19:'5',  39:'6', 34:'/', 50:':', 55:';', 71:"'", 66:'"',
             22:'7',  21:'8',  38:'9', 37:'?', 53:'!', 54:',', 70:'.', 69:'SPEAK',
                       7:'0',
-    })
+    }
 
 _KC_LEFT_SHIFT = const(23)
 _KC_SHIFT = const(36)
 _KC_FN = const(3)
-_KC_CTRL = const(7)
 
 _I2C_ADDR = const(0x55)
 
@@ -71,17 +69,16 @@ class Keys:
         self.tb_left = Pin(1, mode=Pin.IN, pull=Pin.PULL_UP)
         self.tb_right = Pin(2, mode=Pin.IN, pull=Pin.PULL_UP)
         self.tb_click = Pin(0, mode=Pin.IN, pull=Pin.PULL_UP)
-        # irq responds to TB inputs
+        
+        # irq used to count directional input
         self.tb_up.irq(trigger=Pin.IRQ_FALLING, handler=self._handle_irq)
         self.tb_down.irq(trigger=Pin.IRQ_FALLING, handler=self._handle_irq)
         self.tb_left.irq(trigger=Pin.IRQ_FALLING, handler=self._handle_irq)
         self.tb_right.irq(trigger=Pin.IRQ_FALLING, handler=self._handle_irq)
-        self.tb_click.irq(trigger=Pin.IRQ_FALLING, handler=self._handle_irq)
         
         # trackball motion
         self.tb_x = 0
         self.tb_y = 0
-        self.tb_clicks = 0
         
         # set configuration
         self.tb_move_thresh = tb_move_thresh
@@ -98,9 +95,6 @@ class Keys:
             self.tb_y -= 1
         elif tb_pin == self.tb_down:
             self.tb_y += 1
-        
-        elif tb_pin == self.tb_click:
-            self.tb_clicks += 1
 
 
     def _send_code(self, code):
@@ -110,7 +104,7 @@ class Keys:
 
 
     def _add_tb_keys(self, keylist):
-        """Add trackball keys to list"""
+        """Add trackball directions to keylist"""
         tb_x = self.tb_x
         tb_y = self.tb_y
         move_thresh = self.tb_move_thresh
@@ -129,55 +123,49 @@ class Keys:
             else:
                 keylist.append("UP")
         
-        if self.tb_clicks:
-            keylist.append("G0")
-        
-        self.tb_clicks = 0
         self.tb_x = 0
         self.tb_y = 0
         
         
-    def _special_mod_keys(self, keylist):
+    def _special_mod_keys(self, codes, keylist):
         """Convert device-specific key combos into general keys."""
         # shortcut for "OPT" key
-        while "LSHFT" in keylist:
-            keylist.remove("LSHFT")
-            if "CTRL" in keylist:
-                keylist.remove("CTRL")
-                keylist.append("OPT")
-                
-        # finally, remove pre-processed special keys
-        while "SHFT" in keylist:
-            keylist.remove("SHFT")
-        while "FN" in keylist:
-            keylist.remove("FN")
+        if "G0" in keylist \
+        and _KC_FN in codes:
+            keylist.remove("G0")
+            keylist.append("OPT")
 
 
-    def get_pressed_keys(self):
+    def get_pressed_keys(self, force_fn=False, force_shft=False):
         """Return currently pressed keys."""
         codes = self.i2c.readfrom(_I2C_ADDR, _NUM_READ_KEYS)
+        tb_val = self.tb_click.value()
         keys = []
         
-        if codes == _EMPTY_BYTES:
+        # early return on no input
+        if codes == _EMPTY_BYTES \
+        and not self.tb_x \
+        and not self.tb_y \
+        and tb_val:
             return keys
-
+        
         # process special keys before converting to readable format
-        if _KC_FN in codes:
+        if _KC_FN in codes or force_fn:
             keymap = KEYMAP_FN
-        elif _KC_SHIFT in codes \
-        or (_KC_LEFT_SHIFT in codes and _KC_CTRL not in codes):
+        elif _KC_SHIFT in codes or _KC_LEFT_SHIFT in codes or force_shft:
             keymap = KEYMAP_SHFT
         else:
             keymap = KEYMAP
         
-        for code in codes:
-            if code != 0:
-                # avoid rare duplicate keys
-                key = keymap[code]
-                if key not in keys:
-                    keys.append(key)
+        # tb button
+        if tb_val == 0:
+            keys.append("G0")
         
-        self._special_mod_keys(keys)
+        for code in codes:
+            if code != 0 and code in keymap.keys():
+                    keys.append(keymap[code])
+        
+        self._special_mod_keys(codes, keys)
         self._add_tb_keys(keys)
         return keys
         
