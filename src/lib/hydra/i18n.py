c96a9bb9c8f95aa3081863966457cfe892b9e978
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-09-19 13:20:14 -0700

    Linted hydra.i18n

--- src/lib/hydra/i18n.py
+++ src/lib/hydra/i18n.py
@@ -1,31 +1,36 @@
-'''
-This module implements a lightweight internationalization class, 
+"""Internationalization.
+
+This module implements a lightweight internationalization class,
 which can be used to implement translations in MicroHydra.
-'''
+"""
+
+import json
 
 from .config import Config
-import json
 
 
 class I18n:
-    """
-    Internationalization class.
+    """Internationalization class.
 
     args:
     - translations:
         A json string defining a list of dicts,
-        where each dict is formatted like {'lang':'translation', }.
+        where each dict is formatted like `{'lang':'translation', ...}`.
         Example:
         '''[
         {"en": "Loading...", "zh": "加载中...", "ja": "読み込み中..."},
         {"en": "Files", "zh": "文件", "ja": "ファイル"}
         ]'''
-
     """
+
     def __init__(self, translations, key='en'):
-        '''
-        This function initializes the I18n class.
-        '''
+        """Initialize the I18n class.
+
+        Translations are provided with the 'translations' parameter,
+        and will be processed into a single dictionary.
+        'key' selects the language to use as the dictionary key,
+        and the values of that dictionary are based on the language set in 'config.json'.
+        """
         # extract lang from config
         config = Config.instance if hasattr(Config, 'instance') else Config()
         self.lang = config['language']
@@ -34,8 +39,5 @@ class I18n:
         self.translations = {item[key]:item.get(self.lang, item[key]) for item in json.loads(translations)}
 
     def __getitem__(self, text):
-        """
-        Get the translation for the given text,
-        defaulting to the given text.
-        """
+        """Get the translation for the given text, defaulting to the given text."""
         return self.translations.get(text, text)
