a4c3b4202def26c60928a190e6b72c2cfd10a733
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-09-19 12:25:47 -0700

    Linted Display.palette

--- src/lib/display/palette.py
+++ src/lib/display/palette.py
@@ -1,6 +1,7 @@
-"""
-This Palette class is designed to be used for storing a list of RGB565 colors,
-and for returning the apropriate colors by index to be used with MicroHydra's display module.
+"""Provides the Palette class for storing a list of RGB565 colors.
+
+The Palette class is designed to be used for storing a list of RGB565 colors,
+and for returning the appropriate colors by index to be used with MicroHydra's display module.
 
 Key notes on Palette:
   - uses a bytearray to store color information,
@@ -8,30 +9,29 @@ Key notes on Palette:
 
   - Returns an RGB565 color when using normal framebuffer, or an index of the color if use_tiny_buf.
     (This makes it so that you can pass a `Palette[i]` to the Display class in either mode.)
-    
+
   - Palette is a singleton, which is important so that different MH classes can modify and share it's data
     (without initializing the Display).
 """
 
 # Palette class
 class Palette:
+    """Stores the user color palette and converts the values for lib.Display."""
+
     use_tiny_buf = False
     buf = bytearray(32)
-    def __new__(cls):
+    def __new__(cls):  # noqa: D102
         if not hasattr(cls, 'instance'):
-          cls.instance = super(Palette, cls).__new__(cls)
+          cls.instance = super().__new__(cls)
         return cls.instance
-
 
     def __len__(self) -> int:
         return len(self.buf) // 2
-
 
     @micropython.viper
     def __setitem__(self, key:int, new_val:int):
         buf_ptr = ptr16(self.buf)
         buf_ptr[key] = new_val
-
 
     @micropython.viper
     def __getitem__(self, key:int) -> int:
@@ -41,8 +41,7 @@ class Palette:
 
         buf_ptr = ptr16(self.buf)
         return buf_ptr[key]
-    
-    
+
     def __iter__(self):
         for i in range(len(self)):
             yield self[i]
