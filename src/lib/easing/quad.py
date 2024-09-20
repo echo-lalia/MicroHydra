56aab0a99d6bbf515602df65676788ab4e4472c2
echo-lalia <echophenomena@proton.me>
2024-09-20 13:19:41 -0700

    Added easing functions

--- src/lib/easing/quad.py
+++ src/lib/easing/quad.py
@@ -1,22 +1,25 @@
-"""Sine easing functions.
+"""Quad easing functions.
 
 All of these functions take and return a float from 0.0 to 1.0
 and are based on Penner's easing functions,
 and easings.net
 """
-import math
 
 
-def ease_in_sine(x: float) -> float:
+def ease_in_quad(x: float) -> float:
     """Apply ease in function to the given float."""
-    return 1 - math.cos((x * math.pi) / 2)
+    return x * x
 
 
-def ease_out_sine(x: float) -> float:
+def ease_out_quad(x: float) -> float:
     """Apply ease out function to the given float."""
-    return math.sin((x * math.pi) / 2)
+    return 1 - (1 - x) * (1 - x)
 
 
-def ease_in_out_sine(x: float) -> float:
+def ease_in_out_quad(x: float) -> float:
     """Apply ease in/out function to the given float."""
-    return -(math.cos(math.pi * x) - 1) / 2
+    return (
+        2 * x * x
+        if x < 0.5 else
+        1 - (-2*x + 2)**2 / 2
+    )
