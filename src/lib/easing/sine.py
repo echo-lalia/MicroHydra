ea81ce0a64a7d543049c2a132c66d364ee506e77
echo-lalia <echophenomena@proton.me>
2024-09-20 08:28:25 -0700

    Added easing.sine

---
+++ src/lib/easing/sine.py
@@ -0,0 +1,13 @@
+import math
+
+def ease_in_sine(x: float) -> float:
+    return 1 - math.cos((x * math.pi) / 2)
+
+
+def ease_out_sine(x: float) -> float:
+    return math.sin((x * math.pi) / 2)
+
+
+def ease_in_out_sine(x: float) -> float:
+    """Apply an easing function to given float."""
+    return -(math.cos(math.pi * x) - 1) / 2
