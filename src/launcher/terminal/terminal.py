0621567e55a7bff99db75c68484089df843a4cb6
echo-lalia <echophenomena@proton.me>
2024-09-26 23:36:42 -0700

    Add terminal

---
+++ src/launcher/terminal/terminal.py
@@ -0,0 +1,72 @@
+"""Terminal class to write and display to."""
+from lib.display import Display
+from launcher.terminal.termline import TermLine
+from lib.hydra.utils import get_instance
+import os
+
+_MH_DISPLAY_WIDTH = const(240)
+_MH_DISPLAY_HEIGHT = const(135)
+
+_NUM_PRINT_LINES = const(_MH_DISPLAY_HEIGHT // 10)
+_PRINT_LINE_START = const(_MH_DISPLAY_HEIGHT - 12 - (_NUM_PRINT_LINES * 10))
+_MAX_TEXT_WIDTH = const(_MH_DISPLAY_WIDTH // 8)
+
+class Terminal:
+    def __init__(self):
+        self.lines = [TermLine('')] * _NUM_PRINT_LINES
+        self.display = get_instance(Display)
+
+    @staticmethod
+    def split_lines(text:str, max_length:int=_MAX_TEXT_WIDTH) -> list[str]:
+        """Split a string into multiple lines, based on max line-length."""
+        lines = []
+        current_line = ''
+        words = text.split()
+
+        for word in words:
+            while len(word) >= max_length:
+                lines.append(current_line)
+                current_line = word[:max_length]
+                word = word[max_length:]                  
+            if len(word) + len(current_line) >= max_length:
+                lines.append(current_line)
+                current_line = word
+            elif len(current_line) == 0:
+                current_line += word
+            else:
+                current_line += ' ' + word
+
+        lines.append(current_line) # add final line
+
+        return lines
+
+    def print(self, *args, **kwargs):
+        text = ' '.join(args)
+        lines = self.split_lines(text)
+        for line in lines:
+            self.lines.append(TermLine(line))
+            self.lines.pop(0)
+    
+    def draw(self):
+        y = _PRINT_LINE_START
+        for line in self.lines:
+            line.draw(0, y, self.display)
+            y += 10
+
+
+if __name__ == '__main__':
+    from lib.hydra.config import Config
+    config = Config()
+    display = Display()
+    
+    term = Terminal()
+    
+    term.print('HELLO, WORLD!!!')
+    term.print('\x1b[6;30;42m' + 'Success!' + '\x1b[0m')
+    term.print('The quick brown fox jumped over the lazy dog. Also this is some additional text! ertyuhlgfhdseryu5thlkjvmgcfdhrtutiylhkb,vmcngfdtyuiçjl,hvg')
+    term.print('!@#$%*() ´`~^°w?ç§ºª')
+    term.draw()
+    display.show()
+    
+    
+
--- src/launcher/terminal/termline.py
+++ src/launcher/terminal/termline.py
@@ -1,4 +1,5 @@
 """Lines class for the Terminal."""
+from lib.display import Display
 
 
 txt_clrs = {
@@ -23,12 +24,13 @@ txt_clrs = {
 
 
 class _StyleStr:
-    """Color/style and string, for the Line class."""
+    """Color/style and string, for the TermLine class."""
 
     # class level style helps us remember styling between strings/lines
     text_color = None
     bg_color = None
     bold = False
+    underline = False
     def __init__(self, text):
         if text.startswith('\033[') and 'm' in text:
             # Get style attributes
@@ -38,12 +40,12 @@ class _StyleStr:
 
             for styl in style.split(';'):
 
-                print(f'{styl=}')
                 # Style resetters:
                 if styl == '0':  # Reset all
                     _StyleStr.text_color = None
                     _StyleStr.bg_color = None
                     _StyleStr.bold = False
+                    _StyleStr.underline = False
                 elif styl == '39':  # Default foreground color
                     _StyleStr.text_color = None
                 elif styl == '49':  # Default background color
@@ -51,6 +53,13 @@ class _StyleStr:
 
                 elif styl == '1':  # Bold
                     _StyleStr.bold = True
+                elif styl == '22':  # Normal intensity
+                    _StyleStr.bold = False
+                
+                elif styl == '4':  # Underline
+                    _StyleStr.underline = True
+                elif styl == '24':  # Underline off
+                    _StyleStr.underline = False
 
                 # text color
                 elif styl in txt_clrs:
@@ -63,10 +72,23 @@ class _StyleStr:
         self.text = text
         self.txt_clr = _StyleStr.text_color
         self.bg_clr = _StyleStr.bg_color
-        self.bold = _StyleStr.bold
+        self.bld = _StyleStr.bold
+        self.undrln = _StyleStr.underline
+        self.width = Display.get_total_width(text)
+
+    
+    def draw(self, x, y, display):
+        txt_clr = display.palette[8] if self.txt_clr is None else self.txt_clr
+        bg_clr = display.palette[2] if self.bg_clr is None else self.bg_clr
+        display.rect(x, y-1, self.width, 10, bg_clr, fill=True)
+        if self.bld:
+            display.text(self.text, x+1, y, txt_clr)
+        if self.undrln:
+            display.hline(x, y+9, self.width, txt_clr)
+        display.text(self.text, x, y, txt_clr)
 
 
-class Line:
+class TermLine:
     """Single line, with color support."""
 
     def __init__(self, text):
@@ -74,7 +96,7 @@ class Line:
         self.strings = self._get_strings(text)
 
     @staticmethod
-    def get_strings(string) -> list[_StyleStr]:
+    def _get_strings(string) -> list[_StyleStr]:
         """Get style strings from text."""
         strings = []
         current_str = ''
@@ -87,3 +109,9 @@ class Line:
             string = string[1:]
         strings.append(_StyleStr(current_str))
         return strings
+    
+    def draw(self, x, y, display):
+        for string in self.strings:
+            string.draw(x, y, display)
+            x += string.width
+
