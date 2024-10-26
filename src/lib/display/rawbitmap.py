763c4e1304c40652ab3edcbecf9be68f8b156cb6
echo-lalia <108598670+echo-lalia@users.noreply.github.com>
2024-10-26 15:15:13 -0700

    Rework rawbitmap, use in launcher

--- src/lib/display/rawbitmap.py
+++ src/lib/display/rawbitmap.py
@@ -5,46 +5,37 @@ import os
 class RawBitmap:
     """Open a raw bitmap file for use with the Display core."""
 
-    # Cache data for a single bitmap (within a specified size)
-    cached_file = None
+    cached_path = None
     cache = None
-    mv = None
 
     def __init__(self, file_path: str, width: int, height: int, palette: list[int, ...]):
         """Construct the bitmap from given file."""
         self.WIDTH = width
         self.HEIGHT = height
         self.PALETTE = palette
+
         # This assumes that the bits per pixel is always the minimum possible:
-        self.BPP = self._find_bpp(len(palette))
+        self.BPP = 1
+        while len(palette) > (1 << self.BPP):
+            self.BPP += 1
 
-        # The size of the file in bytes
-        self.size = os.stat(file_path)[6]
-        # Open the file for reading
-        self.f = open(file_path, 'rb')  # noqa: SIM115
-        self.deinit = self.f.close
+        if RawBitmap.cached_path == file_path:
+            # Use the cached buffer rather than reloading
+            self.size = len(RawBitmap.cache)
+            self.BITMAP = RawBitmap.cache
+        else:
+            # Load and cache a new buffer
+            self.size = os.stat(file_path)[6]
+            with open(file_path, 'rb') as f:
+                buf = bytearray(self.size)
+                f.readinto(buf)
+                self.BITMAP = memoryview(buf)
 
+            RawBitmap.cached_path = file_path
+            RawBitmap.cache = self.BITMAP
 
-    @staticmethod
-    @micropython.viper
-    def _find_bpp(count: int) -> int:
-        """Find the minimum bits that can be used to encode count colors."""
-        bpp = 1
-        while (1 << bpp) < count:
-            bpp += 1
-        return bpp
-
-
-    def __getattr__(self, key):
-        """Cache the bitmap, and return the cache."""
-        if key != "BITMAP":
-            raise AttributeError(key)
-
-        # Cache this bitmap:
-        if RawBitmap.cached_file is not self.f:
-            RawBitmap.cache = bytearray(self.size)
-            self.f.seek(0)
-            self.f.readinto(RawBitmap.cache)
-            RawBitmap.mv = memoryview(RawBitmap.cache)
-
-        return RawBitmap.mv
+    @classmethod
+    def clean(cls):
+        """Clear the bitmap cache."""
+        cls.cached_path = None
+        cls.cache = None
